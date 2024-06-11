03b616961e20ddff57fda3071e6fe45573385544
KhasanovTR <khasanovtimur2001@gmail.com>
2024-06-11 10:20:21 +0400

    1. Добавлен Liquibase к проекту.

--- scripts422.sql
+++ scripts422.sql
@@ -3,7 +3,7 @@ id smallserial primary key,
 name text not null,
 age smallint,
 driver_license boolean,
-id_car references car(id);
+id_car references car(id));
 create table car(
 id smallserial primary key,
 brand text not null,
