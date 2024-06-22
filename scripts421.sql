85b1157c6908070d602c7b54d50e40ad46a9c0e5
Pupuskjaer <khasanovtimur2001@gmail.com>
2024-06-22 19:05:37 +0400

    Выполнены все задания по параллельным стримам

--- scripts421.sql
+++ scripts421.sql
@@ -1,8 +1,8 @@
 alter table student
-add constraint age_constraint check ( age > 16 ),
+add constraint age_constraint check( age > 16 ),
 alter column name set not null,
-add constraint name_unique unique if  (name),
-alter column age set DEFAULT 20;
+add constraint name_unique unique (name),
+alter column age set DEFAULT 20:
 
 alter table faculty
-add constraint name_color_unique unique (name,color);
+add constraint name_color_unique unique(name,color);
